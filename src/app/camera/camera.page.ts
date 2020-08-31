036d23494a7fc67f33ac6f85794f8972f294f215
ThanhNam20 <khuatthanhnam@gmail.com>
2020-08-31 16:17:49 +0700

    Check

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -1,3 +1,4 @@
+import { Camera } from '@ionic-native/camera/ngx';
 import { CameraService } from './../service/camera.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,16 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./camera.page.scss'],
 })
 export class CameraPage implements OnInit {
-  constructor(private cameraService: CameraService) { }
+  constructor(
+    private camera: Camera,
+  ) { }
   avatar: any;
-  async takePhoto() {
-    await this.cameraService.setAvatar();
-    this.avatar = localStorage.getItem('image');
-    console.log(this.avatar);
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 70,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    };
+    this.camera.getPicture(options).then(data => {
+      this.avatar = data;
+      console.log(this.avatar);
+    });
   }
 
   async getPhoto() {
-    await this.cameraService.getPhotoGallery();
     this.avatar = localStorage.getItem('image-gallery');
     console.log(this.avatar);
   }
--- src/app/service/camera.service.ts
+++ src/app/service/camera.service.ts
@@ -1,13 +1,16 @@
+import { async } from '@angular/core/testing';
 import { Injectable } from '@angular/core';
 import { SafeResourceUrl } from '@angular/platform-browser';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { imageFireStorage, imageStorage } from '../../../firebase/config';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CameraService {
   constructor(private camera: Camera) { }
-  image: any ;
+  image: any;
   setAvatar() {
     const options: CameraOptions = {
       quality: 70,
@@ -15,9 +18,10 @@ export class CameraService {
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
     };
-    this.camera.getPicture(options).then((imageData) => {
-      this.image = 'data:image/jpeg;base64,' + imageData;
-      localStorage.setItem('image', `${this.image}`);
+    this.camera.getPicture(options).then(async (imageData) => {
+      const storageRef = imageStorage.ref(imageData);
+      const url = await storageRef.getDownloadURL();
+      console.log(url);
     }, (err) => {
       console.log(err);
     });
@@ -30,9 +34,10 @@ export class CameraService {
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
       saveToPhotoAlbum: false
     };
-    this.camera.getPicture(options).then((imageData) => {
-      this.image = 'data:image/jpeg;base64,' + imageData;
-      localStorage.setItem('image-gallery', `${this.image}`);
+    this.camera.getPicture(options).then(async (imageData) => {
+      const storageRef = imageStorage.ref(imageData);
+      const url = await storageRef.getDownloadURL();
+      console.log(url);
     }, (err) => {
       console.log(err);
     });
